88b496be58bed2452e7084d8a0f20a2f18c0fb25
chipsy1992 <stefan.chipsy@gmail.com>
2022-06-07 00:20:26 +0200

    docs: add popup for pets cards

--- pages/main/app.js
+++ pages/main/app.js
@@ -13,3 +13,74 @@ function show() {
 function close() {
   mainMenu.style.left = "100%";
 }
+
+// popup
+
+fetch("./pets.json") //path to the file with json data
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => {
+    const btns = document.querySelectorAll(".card-btn");
+
+    [...btns].forEach((elem) => {
+      elem.addEventListener("click", (e) => {
+        const myData = data.find(
+          (elem) => elem.id === e.currentTarget.dataset.btnId
+        );
+        const {
+          name,
+          img,
+          type,
+          breed,
+          description,
+          age,
+          inoculations,
+          diseases,
+          parasites,
+        } = myData;
+
+        let container = document.querySelector(".pets-container");
+
+        const section = document.createElement("div");
+        section.className = "modal";
+        container.append(section);
+        section.innerHTML = `
+    <div class="popup">
+      <div class="show-container">
+        <div class="show-img">
+          <img src="${img}" alt="">
+        </div>
+        <div class="show-content">
+          <h2 class="pet-name">${name}</h2>
+          <h3 class="pet-type">${type} - ${breed}</h3>
+          <p class="pet-description">${description}</p>
+        <ul>
+          <li class="age"><b>Age:</b> ${age}</li>
+          <li class="inoculations"><b>Inoculations:</b> ${inoculations}</li>
+          <li class="diseases"><b>Diseases:</b> ${diseases}</li>
+          <li class="parasites"><b>Parasites:</b> ${parasites}</li>
+        </ul>
+        </div>
+      </div>
+      <span onclick="removeModal(this)" class="close">
+        <i class="fa-solid fa-xmark"></i>
+      </span>
+      </div>
+    <div class="overlay"></div>
+    `;
+
+        let modal = document.querySelector(".popup");
+        let overlay = document.querySelector(".overlay");
+        modal.style.display = "block";
+        overlay.style.display = "block";
+      });
+    });
+  });
+
+function removeModal() {
+  let modal = document.querySelector(".modal");
+  let overlay = document.querySelector(".overlay");
+  modal.remove();
+  overlay.style.display = "none";
+}
